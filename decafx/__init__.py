b89a9397b2f9b81fba8c8ccb6999c6fbf363b1fa
nchlis <nchlis@isc.tuc.gr>
2017-06-13 17:03:41 +0200

    updated image import to use opencv for 16bit tif images

--- decafx/__init__.py
+++ decafx/__init__.py
@@ -5,7 +5,7 @@ Created on Mon Feb  6 12:31:21 2017
 @author: N.Chlis
 """
 
-from .import_ideas import import_ideas, im_adjust
+from .dxio import import_ideas, im_adjust
 from .cae_architectures import (get_encoder, cae_indepIn)
 
 if __name__ == '__main__':
