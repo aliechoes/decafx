dc3e34b7add7483f0cb64eb4075a1ba39ab69e8a
nchlis <nchlis@isc.tuc.gr>
2017-06-13 16:59:36 +0200

    added lambda layer to support only 1 input channel in CAE

--- decafx/cae_architectures.py
+++ decafx/cae_architectures.py
@@ -14,18 +14,20 @@ from keras.layers import Dense
 from keras.layers import Flatten
 from keras.layers import Reshape
 from keras.layers import merge
-from keras.layers.convolutional import Convolution2D
+from keras.layers import Lambda
+from keras.layers.convolutional import Conv2D as Convolution2D
 from keras.layers.convolutional import MaxPooling2D
 from keras.layers.convolutional import UpSampling2D
+from keras.layers.merge import concatenate #Concatenate (capital C) not working 
 
 #%%
 def get_encoder(autoencoder):
-        encoder = Model(input=autoencoder.input, output=autoencoder.get_layer(name='encoder').output)
+        encoder = Model(inputs=autoencoder.input, outputs=autoencoder.get_layer(name='encoder').output)
         return(encoder)
     
 #%%
 def cae_indepIn(nchannels=2, imsize=(32,32), encoding_dim_multiplier=8,
-                  inner_activ='tanh', output_activ='linear'):
+                  inner_activ='relu', output_activ='linear', mode='tf'):
     """
     Convolutional Auto Encoder for feature image extraction.
     All image channels are encoded indepedently, so there is
@@ -81,56 +83,66 @@ def cae_indepIn(nchannels=2, imsize=(32,32), encoding_dim_multiplier=8,
 #    conv2b = [None]*nchannels
 #    bnrm2b = [None]*nchannels
 #    actv2b = [None]*nchannels
-    
+    if mode=='tf':#Tensorflow format
+            shape_in=(imsize[0], imsize[1], 1)
+            bnorm_axis=-1
+    else:#Theano format
+            shape_in=(1, imsize[0], imsize[1])
+            bnorm_axis=1
+            
     # encoder: channel independent convolutions 
     for i in range(nchannels):
-        ch_in[i]=Input(shape=(1, imsize[0], imsize[1]),name='channel'+str(i)+'_input')#32x32
-        conv0[i]=Convolution2D(64, 3, 3, border_mode='same', name='conv0_ch'+str(i))(ch_in[i])
-        bnrm0[i]=BatchNormalization(mode=0, axis=1)(conv0[i])
+        ch_in[i]=Input(shape=shape_in, name='channel'+str(i)+'_input')#32x32#Tensorflow
+        conv0[i]=Convolution2D(64, (3, 3), padding='same', name='conv0_ch'+str(i))(ch_in[i])
+        bnrm0[i]=BatchNormalization(axis=bnorm_axis)(conv0[i])
         actv0[i]=Activation(inner_activ)(bnrm0[i])
-        conv0b[i]=Convolution2D(64, 3, 3, border_mode='same', name='conv0b_ch'+str(i))(actv0[i])
-        bnrm0b[i]=BatchNormalization(mode=0, axis=1)(conv0b[i])
+        conv0b[i]=Convolution2D(64, (3, 3), padding='same', name='conv0b_ch'+str(i))(actv0[i])
+        bnrm0b[i]=BatchNormalization(axis=bnorm_axis)(conv0b[i])
         actv0b[i]=Activation(inner_activ)(bnrm0b[i])
-        pool0[i]=MaxPooling2D((2, 2))(actv0b[i])#16x16
+        pool0[i]=MaxPooling2D((2, 2),)(actv0b[i])#16x16
 
-        conv1[i]=Convolution2D(32, 3, 3, border_mode='same', name='conv1_ch'+str(i))(pool0[i])
-        bnrm1[i]=BatchNormalization(mode=0, axis=1)(conv1[i])
+        conv1[i]=Convolution2D(32, (3, 3), padding='same', name='conv1_ch'+str(i))(pool0[i])
+        bnrm1[i]=BatchNormalization(axis=bnorm_axis)(conv1[i])
         actv1[i]=Activation(inner_activ)(bnrm1[i])
-        conv1b[i]=Convolution2D(32, 3, 3, border_mode='same', name='conv1b_ch'+str(i))(actv1[i])
-        bnrm1b[i]=BatchNormalization(mode=0, axis=1)(conv1b[i])
+        conv1b[i]=Convolution2D(32, (3, 3), padding='same', name='conv1b_ch'+str(i))(actv1[i])
+        bnrm1b[i]=BatchNormalization(axis=bnorm_axis)(conv1b[i])
         actv1b[i]=Activation(inner_activ)(bnrm1b[i])
-        pool1[i]=MaxPooling2D((2, 2))(actv1b[i])#8x8
+        pool1[i]=MaxPooling2D((2, 2),)(actv1b[i])#8x8
 
-        conv2[i]=Convolution2D(encoding_dim_multiplier, 3, 3, border_mode='same', name='conv2_ch'+str(i))(pool1[i])
-        bnrm2[i]=BatchNormalization(mode=0, axis=1)(conv2[i])
+        conv2[i]=Convolution2D(encoding_dim_multiplier, (3, 3), padding='same', name='conv2_ch'+str(i))(pool1[i])
+        bnrm2[i]=BatchNormalization(axis=bnorm_axis)(conv2[i])
         actv2[i]=Activation(inner_activ)(bnrm2[i])
-        pool2[i]=MaxPooling2D((2, 2))(actv2[i])#4x4
+        pool2[i]=MaxPooling2D((2, 2),)(actv2[i])#4x4
     
     # merge the features from all channels
-    conv_merge=merge(pool2,mode='concat',concat_axis=1, name='encoder')
+#    conv_merge=merge(pool2,mode='concat',concat_axis=1, name='encoder')
+    if(nchannels>1):    
+        conv_merge=concatenate(pool2, axis=-1, name='encoder')#merge channels
+    else:
+        conv_merge=Lambda(lambda x: x, name='encoder')(pool2[0])#only 1 channel
     
     # decoder: introduce channel interactions
-    x=Convolution2D(encoding_dim_multiplier+1, 3, 3, border_mode='same')(conv_merge)
-    x=BatchNormalization(mode=0, axis=1)(x)
+    x=Convolution2D(encoding_dim_multiplier+1, (3, 3), padding='same')(conv_merge)
+    x=BatchNormalization(axis=bnorm_axis)(x)
     x=Activation(inner_activ)(x)
-    x=UpSampling2D((2, 2))(x)#8x8
+    x=UpSampling2D((2, 2),)(x)#8x8
     
-    x=Convolution2D(32, 3, 3, border_mode='same')(x)
-    x=BatchNormalization(mode=0, axis=1)(x)
+    x=Convolution2D(32, (3, 3), padding='same')(x)
+    x=BatchNormalization(axis=bnorm_axis)(x)
     x=Activation(inner_activ)(x)
-    x=Convolution2D(32, 3, 3, border_mode='same')(x)
-    x=BatchNormalization(mode=0, axis=1)(x)
+    x=Convolution2D(32, (3, 3), padding='same')(x)
+    x=BatchNormalization(axis=bnorm_axis)(x)
     x=Activation(inner_activ)(x)
-    x=UpSampling2D((2, 2))(x)#16x16
+    x=UpSampling2D((2, 2),)(x)#16x16
     
-    x=Convolution2D(64, 3, 3, border_mode='same')(x)
-    x=BatchNormalization(mode=0, axis=1)(x)
+    x=Convolution2D(64, (3, 3), padding='same')(x)
+    x=BatchNormalization(axis=bnorm_axis)(x)
     x=Activation(inner_activ)(x)
-    x=Convolution2D(64, 3, 3, border_mode='same')(x)
-    x=BatchNormalization(mode=0, axis=1)(x)
+    x=Convolution2D(64, (3, 3), padding='same')(x)
+    x=BatchNormalization(axis=bnorm_axis)(x)
     x=Activation(inner_activ)(x)
-    x=UpSampling2D((2, 2))(x)#32x32
+    x=UpSampling2D((2, 2),)(x)#32x32
     
-    out=Convolution2D(2, 3, 3, border_mode='same',activation=output_activ,name='output')(x)
-    model=Model(input=ch_in,output=out)
+    out=Convolution2D(nchannels, (3, 3), padding='same',activation=output_activ,name='output')(x)
+    model=Model(inputs=ch_in,outputs=out)
     return model
